64fdcff436da02d13ae63b1c4f9170132fe2696e
cyan <cyan.dai@gmail.com>
2011-10-04 22:21:55 -0400

    Primitive Guesser
    The basic algorithm is as follows:
    
    1. shuffle the numbers [1..n]
    2. divide them into pairs
    3. query all pairs
    4. find "maximal" disjoint cover (not optimal yet. I just use an
    agglomerate approach. It can be improved next time.)
    5. query by selecting one element from each disjoint set
    6. solve all pairs in the disjoint cover.
    7. go back to step 4
    
    Future improvement:
    1. optimize the maximal cover
    2. better handle the case when the pairs share one element.
    3. better handle the case when the pairs share two elements.

--- g3/TeamMegamindGuesser.java
+++ g3/TeamMegamindGuesser.java
@@ -1,137 +1,285 @@
 package mapthatset.g3;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Random;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Queue;
 import java.util.Set;
-import java.util.Collections;
 
 import mapthatset.sim.Guesser;
 import mapthatset.sim.GuesserAction;
 
-//if you can see this comment, you have the latest version
-
 public class TeamMegamindGuesser extends Guesser {
 
-	int sizeOfMapping;
-	int lastQueryIndex = 0;
-	ArrayList<ArrayList<Integer>> startingPairGuesses = new ArrayList<ArrayList<Integer>>();
-	String strID = "Team Megamind";
-	// possibilityTable
-	// unknown value is -1, false is 0, true is 1
-	ArrayList<ArrayList<Integer>> possibilityTable = new ArrayList<ArrayList<Integer>>();
-	ArrayList<Integer> lastQuery = new ArrayList<Integer>();
-	ArrayList<Integer> lastResponse = new ArrayList<Integer>();
-	ArrayList<Integer> alGuess = new ArrayList<Integer>();
+	// name of the guesser
+	String strID = "MegamindGuesser";
+	// length of the mapping
+	int MappingLength;
+	// answer[i] is the mapping for i, default value 0 indicates "unknown"
+	ArrayList<Integer> answer;
+	// the number of unknown mapping
+	int unknown;
+	// queue of queries
+	Queue<ArrayList<Integer>> query_queue;
+	// the current query
+	ArrayList<Integer> current_query;
+	// record the query-result pairs
+	Map<HashSet<Integer>, HashSet<Integer>> memory;
+	//
+	Map<HashSet<Integer>, HashSet<Integer>> disjoint_set;
+
+	enum QueryType {
+		Pairs, Disjoint, One, Two
+	}
+
+	// indicator of current query type
+	QueryType current_query_type;
 
 	@Override
 	public void startNewMapping(int intMappingLength) {
-				
-		// initialize the possibilityTable to -1 (unknown everywhere)
-		for (int i = 0; i < intMappingLength; i++) {
-			ArrayList<Integer> row = new ArrayList<Integer>();
-			for (int j = 0; j < intMappingLength; j++) {
-				row.add(-1);
-			}
-			possibilityTable.add(row);
+		this.MappingLength = intMappingLength;
+		this.unknown = intMappingLength;
+
+		// initialize answer to be default value 0
+		this.answer = new ArrayList<Integer>(this.MappingLength + 1);
+		// subscript starts from 1, ignoring 0
+		for (int i = 0; i != this.MappingLength + 1; ++i)
+			this.answer.add(0);
+
+		// get a random permutation of 1..n as shuffledList
+		ArrayList<Integer> shuffledList = new ArrayList<Integer>(
+				intMappingLength);
+		for (int i = 0; i != this.MappingLength; ++i)
+			shuffledList.add(i + 1);
+		java.util.Collections.shuffle(shuffledList);
+
+		// initialize the memory
+		memory = new HashMap<HashSet<Integer>, HashSet<Integer>>();
+
+		// initialize the query queue
+		this.query_queue = new LinkedList<ArrayList<Integer>>();
+
+		// generate all pair queries into query queue
+		Iterator<Integer> iter = shuffledList.iterator();
+		while (iter.hasNext()) {
+			ArrayList<Integer> query = new ArrayList<Integer>();
+			query.add(iter.next());
+			if (iter.hasNext())
+				query.add(iter.next());
+			query_queue.add(query);
 		}
-		
-		sizeOfMapping = intMappingLength;
-		lastQueryIndex = 0;
-		startingPairGuesses = new ArrayList<ArrayList<Integer>>();
-		alGuess = new ArrayList< Integer >();
-		fillStartingPairGuesses();
-	}
-	
-	public void fillStartingPairGuesses() {
-		ArrayList<Integer>randomList = new ArrayList<Integer>();
-		//fill list with 1-n
-		for(int i = 1; i <= sizeOfMapping; i ++)
-			randomList.add(i);
-		//randomize n values
-		Collections.shuffle(randomList);
-		ArrayList<Integer>pair = new ArrayList<Integer>();
-		startingPairGuesses = new ArrayList<ArrayList<Integer>>();
-		//insert random guesses 
-		int i = 1;
-		while(i <= sizeOfMapping) {
-			pair = new ArrayList<Integer>();
-			for(int j = 0; j < 2; j++) {
-				pair.add(randomList.get(i-1));
-				if(i == sizeOfMapping) //in case of odd size
-					j = 3;
-				i++;
-			}
-			System.out.print("pair: " + pair.size() + "\n");
-			//add the set to the arraylist
-			startingPairGuesses.add(pair);
-		}
+
+		current_query_type = QueryType.Pairs;
 	}
 
 	@Override
 	public GuesserAction nextAction() {
-		//guess initial starting pairs
-		lastQueryIndex ++;
-		GuesserAction gscReturn = null;
-		if (lastQueryIndex-1 >= startingPairGuesses.size())
-		{
-			String strGuessing = "";
-			//for ( int intGuessingElement : alGuess )
-			for (int intGuessingElement : alGuess )
-			{
-					strGuessing += intGuessingElement + ", ";
+		// if we know the answer
+		if (unknown == 0) {
+			// remove the first element
+			List<Integer> guess = answer.subList(1, answer.size());
+			answer = new ArrayList<Integer>(guess);
+			return new GuesserAction("g", answer);
+		}
+		if (query_queue.isEmpty())
+			try {
+				this.generateQueries();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			//System.out.println( "Guessing: " + strGuessing.substring( 0, strGuessing.length() - 1 ) );
-			gscReturn = new GuesserAction( "g", alGuess );
-		}
-		else
-		{
-			ArrayList< Integer > alQueryContent = new ArrayList< Integer >();
-			alQueryContent = startingPairGuesses.get(lastQueryIndex-1);
-			gscReturn = new GuesserAction( "q", alQueryContent);
-		}
-		return gscReturn;
+
+		current_query = query_queue.remove();
+		return new GuesserAction("q", current_query);
 	}
 
 	@Override
 	public void setResult(ArrayList<Integer> alResult) {
-		
-		lastResponse.clear();
-		for (int i = 0; i < alResult.size(); i++) {
-			lastResponse.add(alResult.get(i));
-		}
-		// TODO Auto-generated method stub
-		for(int i = 0; i < alResult.size(); i++)
-			alGuess.add( alResult.get( i ) );
 
+		switch (current_query_type) {
+		case Pairs:
+			// special case: same result for current query
+			if (alResult.size() == 1) {
+				int value = alResult.get(0);
+				for (Integer i : current_query)
+					answer.set(i, value);
+				unknown -= current_query.size();
+			}
+			// memorize the result
+			memory.put(new HashSet<Integer>(current_query),
+					new HashSet<Integer>(alResult));
+			break;
+		case Disjoint:
+			resolveDisjoint(alResult);
+			this.updateMemory();
+			System.out.println("After disjoint query, memory:\n"
+					+ this.getMemory());
+			System.out.println("Answer:" + this.getAnswer());
+			current_query_type = QueryType.Pairs;
+			break;
+		}
 	}
 
 	@Override
 	public String getID() {
-		// TODO Auto-generated method stub
-		return strID;
+		return this.strID;
 	}
 
-	private void updatePossibilityTable() {
-		
-		// for every number X queried, update possibilityTable with '1's
-		// at every location in row X, place a '1' in the columns that do not appear in lastResponse
-		for (int i = 0; i < lastQuery.size(); i++) {
-			int row = lastQuery.get(i);
-			for (int j = 0; j < sizeOfMapping; j++){
-				if (!lastResponse.contains(j)) possibilityTable.get(row).set(j, 0);
+	public String getAnswer() {
+		return this.answer.subList(1, answer.size()).toString();
+	}
+
+	public String getQueryQueue() {
+		String s = new String();
+		for (ArrayList<Integer> q : query_queue)
+			s += q.toString() + ' ';
+		return s;
+	}
+
+	public String getMemory() {
+		String s = new String();
+		for (Entry<HashSet<Integer>, HashSet<Integer>> e : memory.entrySet())
+			s += e.getKey().toString() + "->" + e.getValue().toString() + "\n";
+		return s;
+	}
+
+	private void generateQueries() throws Exception {
+		System.out.println("\nMemory:");
+		System.out.print(this.getMemory());
+		System.out.println("\nAnswer:");
+		System.out.println(this.getAnswer());
+
+		switch (current_query_type) {
+		case Pairs: // all pairs
+			disjoint_set = findMaximalDisjointSet();
+			// future improvement: if disjoint_set.size == 1?
+			ArrayList<Integer> query = selectQueryFromDisjointSet(disjoint_set);
+			query_queue.add(query);
+			current_query_type = QueryType.Disjoint;
+
+			System.out.println("Query from disjoint set:");
+			System.out.println(query);
+			break;
+
+		case Disjoint:
+			break;
+
+		case One:
+			break;
+
+		case Two:
+			break;
+
+		default:
+			throw new Exception("Invalid Phase!");
+		}
+	}
+
+	private void updateMemory() {
+		HashSet<HashSet<Integer>> toBeDeleted = new HashSet<HashSet<Integer>>();
+		HashSet<HashMap<Integer, Integer>> new_answers = new HashSet<HashMap<Integer, Integer>>();
+		for (Entry<HashSet<Integer>, HashSet<Integer>> e : memory.entrySet()) {
+			// both query and result must be pairs
+			HashSet<Integer> query = e.getKey();
+			HashSet<Integer> result = e.getValue();
+			for (Integer q : query) {
+				// if we know the answer for q
+				if (answer.get(q) != 0) {
+					HashMap<Integer, Integer> new_answer = resolvePairs(query,
+							result, q);
+					new_answers.add(new_answer);
+					toBeDeleted.add(query);
+				}
+			}
+		}
+
+		// delete resolved pairs
+		for (HashSet<Integer> obsolete : toBeDeleted)
+			memory.remove(obsolete);
+
+		// store new answers
+		for (HashMap<Integer, Integer> as : new_answers) {
+			for (Entry<Integer, Integer> a : as.entrySet()) {
+				answer.set(a.getKey(), a.getValue());
+			}
+		}
+		unknown -= new_answers.size();
+	}
+
+	private HashMap<Integer, Integer> resolvePairs(HashSet<Integer> query,
+			HashSet<Integer> result, int q) {
+		// copy the query and result
+		HashSet<Integer> tmp_query = new HashSet<Integer>(query);
+		HashSet<Integer> tmp_result = new HashSet<Integer>(result);
+
+		// get the other query
+		tmp_query.remove(q);
+		int other = (Integer) tmp_query.toArray()[0];
+
+		// get the other value
+		int value = answer.get(q);
+		tmp_result.remove(value);
+		int other_value = (Integer) tmp_result.toArray()[0];
+
+		System.out.println("I know:" + q + "->" + value);
+		System.out.println("I infer:" + other + "->" + other_value);
+
+		HashMap<Integer, Integer> new_answer = new HashMap<Integer, Integer>();
+		new_answer.put(other, other_value);
+		return new_answer;
+	}
+
+	private Map<HashSet<Integer>, HashSet<Integer>> findMaximalDisjointSet() {
+		// not optimal, using agglomerate cover
+		HashSet<Integer> cover = new HashSet<Integer>();
+		Map<HashSet<Integer>, HashSet<Integer>> disjoint_set = new HashMap<HashSet<Integer>, HashSet<Integer>>();
+		for (Entry<HashSet<Integer>, HashSet<Integer>> e : memory.entrySet()) {
+			HashSet<Integer> tmp_cover = new HashSet<Integer>(cover);
+			HashSet<Integer> result = e.getValue();
+			// intersection
+			tmp_cover.retainAll(result);
+			// if it is disjoint
+			if (tmp_cover.size() == 0) {
+				cover.addAll(result);
+				disjoint_set.put(e.getKey(), e.getValue());
+			}
+		}
+
+		System.out.println("Disjoint Set:");
+		System.out.println(disjoint_set.toString());
+		return disjoint_set;
+	}
+
+	private ArrayList<Integer> selectQueryFromDisjointSet(
+			Map<HashSet<Integer>, HashSet<Integer>> disjoint_set) {
+		ArrayList<Integer> query = new ArrayList<Integer>();
+		for (HashSet<Integer> q : disjoint_set.keySet()) {
+			int anyone = (Integer) q.toArray()[0];
+			query.add(anyone);
+		}
+		return query;
+	}
+
+	private void resolveDisjoint(ArrayList<Integer> result) {
+		System.out.println("Resolve disjoint query:");
+		System.out.println("From result:" + result);
+		for (Integer value : result) {
+			for (Entry<HashSet<Integer>, HashSet<Integer>> e : disjoint_set
+					.entrySet()) {
+				if (e.getValue().contains(value)) {
+					HashSet<Integer> query = new HashSet<Integer>(e.getKey());
+					query.retainAll(current_query);
+					int q = (Integer) query.toArray()[0];
+					answer.set(q, value);
+					unknown -= 1;
+					System.out.println("I infer:" + q + "->" + value);
+					break;
+				}
 			}
 		}
 	}
-	
-	private void printPossibilityTable() {
-		
-		for (int row = 0; row < sizeOfMapping; row++) {
-			for (int col = 0; col < sizeOfMapping; col++) {
-				System.out.print(possibilityTable.get(row).get(col) + " ");
-			}
-			System.out.println();
-		}
-	}
-	
 }

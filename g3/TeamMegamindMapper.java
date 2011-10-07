9c06d8f7a8751150ccae2944b390d10758bec9e8
erikackermann <erik.ackermann@gmail.com>
2011-10-07 15:44:01 -0400

    Added third case to MegamindMapper
    Added the mapping everything -> {a, b} (everything to two elements)

--- g3/TeamMegamindMapper.java
+++ g3/TeamMegamindMapper.java
@@ -9,14 +9,16 @@ public class TeamMegamindMapper extends Mapper {
 
 	int intMappingLength;
 	String strID = "MegamindMapper";
+	int round; // index to keep track of how many games played
+	
 	
 	private ArrayList< Integer > getNewMapping() {
 		
 		
 		ArrayList< Integer > alNewMapping = new ArrayList< Integer >();
 		Random rdmGenerator = new Random();
-		int randomChoice = rdmGenerator.nextInt(2); //creates a random choice of either 0 or 1
-		//int randomChoice = 1;
+		int randomChoice = rdmGenerator.nextInt(3); //creates a random choice of 0, 1 or 2
+		//int randomChoice = 2;
 		
 		switch(randomChoice) {
 		
@@ -24,8 +26,8 @@ public class TeamMegamindMapper extends Mapper {
 			System.out.print("All unique map\n");
 			for ( int intIndex = 0; intIndex < intMappingLength; intIndex ++ ) {
 				alNewMapping.add( intIndex+1 );
-				Collections.shuffle(alNewMapping);
 			}
+			Collections.shuffle(alNewMapping);
 			System.out.println( "The mapping is: " + alNewMapping );
 			break;
 		case 1:
@@ -49,6 +51,24 @@ public class TeamMegamindMapper extends Mapper {
 				alNewMapping.add( rdmGenerator.nextInt( intMappingLength ) + 1 );
 			}
 			//randomize final map before sending it for security reasons
+			Collections.shuffle(alNewMapping);
+			System.out.println( "The mapping is: " + alNewMapping );
+			break;
+		case 2:
+			System.out.print("All keys map to two elements\n");
+			// choose the two numbers to map everything to
+			int firstNumber = rdmGenerator.nextInt(intMappingLength) + 1;
+			int secondNumber = rdmGenerator.nextInt(intMappingLength) + 1;
+			// pick random number between n/4 and 3n/4
+			int split = rdmGenerator.nextInt(intMappingLength/2) + intMappingLength/4;
+			// this number is the frequency of 'firstNumber' in the mapping
+			// firstNumber occurs split times, second number occurs n-split times
+			for ( int intIndex = 0; intIndex < split; intIndex ++ ) {
+				alNewMapping.add( firstNumber );
+			}
+			for ( int intIndex = split; intIndex < intMappingLength; intIndex ++ ) {
+				alNewMapping.add( secondNumber );
+			}
 			Collections.shuffle(alNewMapping);
 			System.out.println( "The mapping is: " + alNewMapping );
 			break;

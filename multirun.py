77d0197e7e8980f4792e3b3ad3ca208d43089891
erikackermann <erik.ackermann@gmail.com>
2011-10-08 20:02:48 -0400

    Updated python script to do multiple runs at each parameter combination, take average
    This script is painfully slow.  Maybe I'll next I'll update the script
    to run the tests in parallel; currently it is doing each test
    sequentially.

--- multirun.py
+++ multirun.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
 	nstep = 5
 	kmax = 20;
 	kstep = 2;
+	runs = 10.0;
 	
 	for i in range(5, nmax+1, nstep):
 		a.append([])
@@ -21,15 +22,19 @@ if __name__ == "__main__":
 		a[row].append(i)
 		col = 0
 		for j in range(2, kmax+1, kstep):
-			command = ["java", "-jar", "mtsProgram.jar", "-x", str(i), str(j)]
-			process = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE)
-			output = process.communicate()[0]
-			pos = -1
-			x = output[-1]
-			while (x != " "):
-				pos -= 1
-				x = output[pos:pos+1]
-			a[row].append(int(output[pos+1:-1]))
+			print "current run: n =", i, "k =", j
+			totalSteps = 0
+			for run in range(0, int(runs)):
+				command = ["java", "-jar", "mtsProgram.jar", "-x", str(i), str(j)]
+				process = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE)
+				output = process.communicate()[0]
+				pos = -1
+				x = output[-1]
+				while (x != " "):
+					pos -= 1
+					x = output[pos:pos+1]
+				totalSteps += int(output[pos+1:-1])
+			a[row].append(totalSteps/runs) # add the average of all the runs to the array
 			col += 1
 		row += 1
 	
